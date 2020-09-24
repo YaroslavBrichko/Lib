f38fc09c20af551058f3590d6c2ad9a1b027bdd9
Yaroslav Brichko <34482995+YaroslavBrichko@users.noreply.github.com>
2020-09-24 14:42:47 +0300

    Add files via upload

---
+++ SimpleCast/Caster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+namespace SimpleCast
+{
+    static class Caster<TFrom, TTo>
+    {
+        public static readonly Func<TFrom, TTo> Cast = compile();
+        private static Func<TFrom, TTo> compile()
+        {
+            var param = Expression.Parameter(typeof(TFrom));
+            var lambda = Expression.Lambda<Func<TFrom, TTo>>(param, param);
+            return lambda.Compile();
+        }
+    }
+}
---
+++ SimpleCast/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Xml;
+
+namespace SimpleCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Data();
+            int i1 = GetData<int>(data);
+            double d = GetData<double>(data);
+            string s = GetData<string>(data);
+            XmlDocument doc = GetData<XmlDocument>(data);
+
+        }
+
+        static T GetData<T>(Data d)
+        {
+            Type type = typeof(T);
+            switch (type)
+            {
+                case Type intType when intType == typeof(int):
+                    return Caster<int, T>.Cast(d.GetInt());
+                case Type doubleType when doubleType == typeof(double):
+                    return Caster<double, T>.Cast(d.GetDouble());
+                case Type stringType when stringType == typeof(string):
+                    return Caster<string, T>.Cast(d.GetString());
+                case Type xmlType when xmlType == typeof(XmlDocument):
+                    return Caster<XmlDocument, T>.Cast(d.GetXml());
+                default:
+                    throw new NotSupportedException();
+            }
+
+        }
+
+        class Data
+        {
+            public XmlDocument GetXml()
+            {
+                return new XmlDocument();
+            }
+            public int GetInt()
+            {
+                return 2;
+            }
+
+            public string GetString()
+            {
+                return "ss";
+            }
+
+            public double GetDouble()
+            {
+                return 2.0;
+            }
+        }
+    }
+}
